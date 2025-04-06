38bbead9a27ab3d04f5cc217fd1d26c12052302b
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-04-07 03:34:53 +0530

    anim : Stars

--- src/pages/LaunchLab.tsx
+++ src/pages/LaunchLab.tsx
@@ -2,12 +2,13 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import "../styles/pages/LaunchLab.css";
 import { useEffect } from "react";
+import Stars from "../components/LaunchLab/Stars";
 // Import Particles component
 
 export default function LaunchLab() {
   const { isLoggedIn, user } = useAuth();
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     if (!isLoggedIn || !user) {
       navigate("/", { replace: true });
@@ -20,27 +21,9 @@ export default function LaunchLab() {
 
       <div className="launch-lab-content">
         <div className="left-pane">
-          <h1 className="launch-lab-title">Launch Lab</h1>
-          <p className="launch-lab-description">
-            Welcome to the Launch Lab! Here, you can explore and experiment with
-            our latest features and tools. Dive in and start creating!
-          </p>
-          <button className="explore-button">Explore Now</button>
-          <div className="launch-lab-features">
-            <h2 className="features-title">Features</h2>
-            <ul className="features-list">
-              <li className="feature-item">
-                Feature 1: AI-Powered Recommendations
-              </li>
-              <li className="feature-item">
-                Feature 2: Real-time Collaboration
-              </li>
-              <li className="feature-item">
-                Feature 3: Customizable Workspaces
-              </li>
-            </ul>
-          </div>
+          <Stars />
         </div>
+
         <div className="right-pane">
           <div className="launch-lab-image-container">
             <img
