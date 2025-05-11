4241cfc3a2b4af143a57acf70bc7c8a2a7607e60
harii55 <hariii5389@gmail.com>
2025-05-12 02:22:33 +0530

    prog  :  added package.json with dependencies and scripts for development, build, linting, and preview.

---
+++ src/newui/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "../../pages/Dashboard";
+import TopicPage from "./client/pages/TopicPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/module/:moduleId/topic/:topicId/subtopic/:subtopicId"
+          element={<TopicPage />}
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
