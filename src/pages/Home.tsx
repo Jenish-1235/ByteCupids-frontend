a87a939f727248a58af2f63f3b4c727a811e584e
Jenish-1235 <jenishtogadiya549@gmail.com>
2025-05-12 19:12:12 +0530

    prog : module tab bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,25 @@ import Sidebar from "../components/Home/Sidebar";
 import Header from "../components/Home/Header";
 import Modules from "../components/Home/Modules";
 import "../styles/pages/Home.css";
+import ModuleTabBar from "../components/Home/ModuleTabBar";
 
+const modules = [
+  "Introduction to React",
+  "DSA",
+  "System Design",
+  "Operating System",
+  "MySQL",
+  "Java",
+  "JavaScript",
+  "Python",
+  "C++",
+  "C",
+  "HTML",
+  "CSS",
+  
+];
 const Home: React.FC = () => {
+  const [activeModule, setActiveModule] = React.useState(modules[0]);
   return (
     <div className="home-page cursor-bg">
       {/* Global glow effects at different positions */}
@@ -18,7 +35,11 @@ const Home: React.FC = () => {
       <Sidebar />
       <main className="main">
         <Header userName="Jenish-1235"/>
-        <Modules />
+        <ModuleTabBar
+        modules={modules}
+        activeModule={activeModule}
+        onModuleChange={setActiveModule}
+        />
       </main>
     </div>
   );
