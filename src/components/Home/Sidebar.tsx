5c42dfe60504b1ea8afd45f58a5cc136fdeb0bff
harii55 <hariii5389@gmail.com>
2025-05-15 02:50:37 +0530

    prog : created ContentMarkDown.tsx and ContentMarkdown.css for styling markdown content

--- src/components/Home/Sidebar.tsx
+++ src/components/Home/Sidebar.tsx
@@ -11,7 +11,13 @@ const Sidebar: React.FC = () => {
         onClick={() => navigate("/")}
       >
         <span className="sidebar__icon">
-          <svg width="25" height="25" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg
+            width="25"
+            height="25"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
               d="M3 9.5L10 4L17 9.5"
               stroke="currentColor"
@@ -30,21 +36,39 @@ const Sidebar: React.FC = () => {
         </span>
         <span className="sidebar__label">Home</span>
       </div>
-      
+
       <div className="sidebar__menu-item">
         <span className="sidebar__icon">
-          <svg width="25" height="25" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="10" cy="10" r="8" stroke="currentColor" strokeWidth="2.2" />
+          <svg
+            width="25"
+            height="25"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              cx="10"
+              cy="10"
+              r="8"
+              stroke="currentColor"
+              strokeWidth="2.2"
+            />
             <rect x="9" y="8" width="2" height="6" rx="1" fill="currentColor" />
             <rect x="9" y="5" width="2" height="2" rx="1" fill="currentColor" />
           </svg>
         </span>
         <span className="sidebar__label">About Us</span>
       </div>
-      
+
       <button className="sidebar__logout-btn">
         <span className="sidebar__icon">
-          <svg width="25" height="25" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg
+            width="25"
+            height="25"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
               d="M13 15L18 10L13 5"
               stroke="currentColor"
